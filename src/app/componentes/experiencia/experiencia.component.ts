04a986e3ac4d092c5516ccf168a6ec9c5c9defe0
meryeliz <lizzybtk@outlook.com>
2022-08-19 00:51:53 -0300

    seccion trabajo

--- src/app/componentes/experiencia/experiencia.component.ts
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/servicios/porfolio.service';
 
 @Component({
   selector: 'app-experiencia',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencia.component.css']
 })
 export class ExperienciaComponent implements OnInit {
+  experienciaLista: any;
 
-  constructor() { }
+  constructor(private datosPorfolio:PorfolioService) { }
 
   ngOnInit(): void {
+    
+    this.datosPorfolio.obtenerDatos().subscribe(data => {
+      this.experienciaLista = data;
+      console.log(this.experienciaLista);
+    })
   }
 
 }
