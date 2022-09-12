fdcc2e465156a798bea0ce19aae99c88147f8e62
meryeliz <lizzybtk@outlook.com>
2022-09-12 19:42:17 -0300

    cambios template login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -12,12 +12,25 @@ export class LoginComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {
     this.form = this.formBuilder.group(
       {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        deviceInfo: this.formBuilder.group({
+          deviceId: ["17867868768"], 
+          deviceType: ["DEVICE_TYPE_ANDROID"],
+          notificationToken: ["67687575eececc34"]
+        })
       
     }
     )
-   }
+  }
 
   ngOnInit(): void {
+  }
+  get Email() {
+    return this.form.get('email');
+  }
+  get Password() {
+    return this.form.get('password')
   }
 
 }
