b18efe1d6197a2c7ec8cc19e220a528a3cd97e2d
meryeliz <lizzybtk@outlook.com>
2022-09-14 01:01:54 -0300

    cambios login

--- src/app/servicios/autenticacion.service.ts
+++ src/app/servicios/autenticacion.service.ts
@@ -21,7 +21,7 @@ export class AutenticacionService {
     //retorna el metodo post
     return this.http.post(this.url, credenciales).pipe(map(data => {
       sessionStorage.setItem('currentUser', JSON.stringify(data));
-
+      this.currentUserSubject.next(data);
 
       return data;
     }))
